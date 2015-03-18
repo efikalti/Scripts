2c6e0ed66cb677509d5611b24ff5945b6c306da1
kaltirim <kaltirim@lab.grid.auth.gr>
2015-03-18 15:37:09 +0200

    add els directory path to bashrc file

---
+++ add-paths.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+file=$HOME/.bashrc
+string='#Elasticsearch path'
+echo "" >> $file
+grep -q "$string" "$file" || echo "$string" >> "$file"
+string='export ELS_HOME=$HOME/elasticsearch'
+grep -q "$string" "$file" || echo "$string" >> "$file"
